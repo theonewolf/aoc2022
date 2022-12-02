c595780b392df50d4dfd0d15524c59ab4cdea59d
Wolfgang Richter <wolf@wolfgroup.llc>
2022-12-01 19:17:17 -0500

    day 1, part 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<List<int>> elves = new List<List<int>>();
+
+List<int> current_elf = new List<int>();
+foreach (string line in System.IO.File.ReadLines(@"./input1")) {
+    if (line != "") {
+        current_elf.Add(int.Parse(line));
+    } else {
+        elves.Add(current_elf);
+        current_elf = new List<int>();
+    }
+}
+
+int max = elves.Max(x => x.Sum());
+Console.WriteLine(max);
