8f86513433b15659850e656ca99f8a6b704e371b
Wolfgang Richter <wolf@wolfgroup.llc>
2022-12-06 19:00:55 -0500

    day 6, part 2

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,15 +1,11 @@
 ﻿foreach (string line in System.IO.File.ReadLines(@"./input")) {
-    Console.WriteLine(StartMarkerPosition(line));
+    Console.WriteLine(StartMarkerPosition(line, 14));
 }
 
-int StartMarkerPosition(string recorded) {
-    // Sliding window for every 4 characters.
-    // If all unique, report position after last unique char in the set of 4
-
-    for (int i = 0; i < recorded.Length - 4; i++) {
-        var (a,b,c,d) = (recorded[i], recorded[i+1], recorded[i+2], recorded[i+3]);
-        if (a != b && a != c && a != d && b != c && b != d && c != d) {
-            return i + 4;
+int StartMarkerPosition(string recorded, int length) {
+    for (int i = 0; i < recorded.Length - length; i++) {
+        if (recorded.Substring(i, length).Distinct().Count() == length) {
+            return i + length;
         }
     }
 
