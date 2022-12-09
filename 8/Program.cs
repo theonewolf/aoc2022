984c8612b9b1f1765ee30a807daaea0ac0fe2ffa
Wolfgang Richter <wolf@wolfgroup.llc>
2022-12-08 20:57:44 -0500

    day 8 part 2

--- 8/Program.cs
+++ 8/Program.cs
@@ -87,4 +87,64 @@ bool IsVisible(int x, int y, List<List<int>> grid) {
     return false;
 }
 
+int maxScenicScore = int.MinValue;
+
+for (int i = 0; i < grid.Count(); i ++) {
+    for (int j = 0; j < grid[0].Count(); j++) {
+        var potentialMax = ScenicScore(i, j, grid);
+        if (potentialMax >= maxScenicScore) {
+            maxScenicScore = potentialMax;
+        }
+    }
+}
+
+int ScenicScore(int x, int y, List<List<int>> grid) {
+    int tree = grid[y][x];
+    int top = 0, bottom = 0, right = 0, left = 0;
+
+    // Edges always have a 0 score
+    if (x == 0 || y == 0 || x == grid[0].Count() - 1 || y == grid.Count() - 1) {
+        return 0;
+    }
+
+    // Check top
+    for (int i = y - 1; i >= 0; i--) {
+        if (grid[i][x] >= tree) {
+            top++;
+            break;
+        }
+        top++;
+    }
+
+    // Check bottom
+    for (int i = y + 1; i < grid.Count(); i++) {
+        if (grid[i][x] >= tree) {
+            bottom++;
+            break;
+        }
+        bottom++;
+    }
+
+    // Check right
+    for (int i = x + 1; i < grid[0].Count(); i++) {
+        if (grid[y][i] >= tree) {
+            right++;
+            break;
+        }
+        right++;
+    }
+
+    // Check left
+    for (int i = x - 1; i >= 0; i--) {
+        if (grid[y][i] >= tree) {
+            left++;
+            break;
+        }
+        left++;
+    }
+
+    return top * bottom * left * right;
+}
+
 Console.WriteLine($"Total visible trees: {total}");
+Console.WriteLine($"Max Scenic Score: {maxScenicScore}");
