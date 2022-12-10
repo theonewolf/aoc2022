bfa24df01db5c2611f4a0c9edb85a7fd8bddca2b
Wolfgang Richter <wolf@wolfgroup.llc>
2022-12-10 08:25:20 -0500

    part 1 day 10 solved awhile ago

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,3 +1,73 @@
-foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
-    Console.WriteLine(line);
+int register_x = 1;
+int cycle_count = 1;
+int signal_strength = 0;
+
+int target_cycle = 20;
+
+List<List<char>> crt_screen = new List<List<char>>();
+
+foreach (string line in System.IO.File.ReadLines(@"./input")) {
+    var splitline = line.Split();
+    var (instruction, value) = (splitline[0], 0);
+
+    if (instruction == "addx") {
+        value = int.Parse(splitline[1]);
+    }
+
+    Execute(instruction, value);
 }
+
+void ClearScreen() {
+    crt_screen.Clear();
+
+    foreach (var _ in Enumerable.Range(0, 6) {
+            var row = new List<char>();
+            crt_screen.Add(new List<char>());
+
+            foreach (var __ in Enumerable.Range(0, 40)) {
+                row.Add('.');
+            }
+    }
+}
+
+void PrintScreen() {
+    foreach (var row in crt_screen) {
+        foreach (var col in row) {
+            Console.Write(col);
+        }
+        Console.WriteLine();
+    }
+}
+
+void IncrementCounter() {
+    if (cycle_count == target_cycle) {
+        signal_strength += cycle_count * register_x;
+        target_cycle += 40;
+        Console.WriteLine($"Signal Strength: {signal_strength}, Cycle Count: {cycle_count}, Register x: {register_x}");
+
+    }
+
+    cycle_count++;
+}
+
+void Execute(string instruction, int arg) {
+    switch(instruction) {
+        case "noop":
+            IncrementCounter();
+            break;
+
+        case "addx":
+            IncrementCounter();
+            IncrementCounter();
+            register_x += arg;
+            break;
+
+        default:
+           throw new NotImplementedException();
+    }
+}
+
+Console.WriteLine($"FINAL | Signal Strength: {signal_strength}, Cycle Count: {cycle_count}, Register x: {register_x}");
+
+ClearScreen();
+PrintScreen();
--- 9/Program.cs
+++ 9/Program.cs
@@ -1,3 +1,76 @@
-foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
-    Console.WriteLine(line);
+Dictionary<(int, int), int> visited_locations = new Dictionary<(int, int), int>();
+int H_x = 0, H_y = 0, T_x = 0, T_y = 0;
+
+foreach (string line in System.IO.File.ReadLines(@"./input")) {
+    var splitline = line.Split();
+    var (direction, count) = (splitline[0], splitline[1]);
+
+    (H_x, H_y, T_x, T_y) = Simulate(H_x, H_y, T_x, T_y, direction, int.Parse(count), visited_locations);
 }
+
+(int, int, int, int) Simulate(int h_x, int h_y, int t_x, int t_y, string direction, int count, Dictionary<(int, int), int> locations)
+{
+    for (int i = 0; i < count; i++) {
+        (h_x, h_y, t_x, t_y) = Step(h_x, h_y, t_x, t_y, direction);
+        if (!locations.ContainsKey((t_x, t_y))) {
+            locations.Add((t_x, t_y), 1);
+        } else {
+            locations[(t_x, t_y)] += 1;
+        }
+    }
+
+    return (h_x, h_y, t_x, t_y);
+}
+
+(int, int, int, int) Step(int h_x, int h_y, int t_x, int t_y, string direction)
+{
+    Console.WriteLine($"Direction: {direction}");
+
+    switch (direction) {
+        case "U":
+            h_y += 1;
+            break;
+        case "D":
+            h_y -= 1;
+            break;
+        case "R":
+            h_x += 1;
+            break;
+        case "L":
+            h_x -= 1;
+            break;
+        default:
+            throw new NotImplementedException();
+    }
+
+    int d_x = h_x - t_x,
+        d_y = h_y - t_y;
+    
+    int distance = (int) Math.Sqrt(d_x * d_x + d_y * d_y);
+
+    Console.WriteLine($"d_x = {d_x}, d_y = {d_y}, Euclidean distance = {distance}");
+
+    if (distance > 1) {
+        // Diagonal move (4 directions)
+        if (Math.Abs(d_x) >= 1 && Math.Abs(d_y) >= 1) {
+            t_x += Math.Sign(d_x) * 1;
+            t_y += Math.Sign(d_y) * 1;
+        // Horizontal move (2 directions)
+        } else if (Math.Abs(d_x) == 2) {
+            t_x += Math.Sign(d_x) * 1;
+        // Vertical move (2 directions)
+        } else if (Math.Abs(d_y) == 2) {
+            t_y += Math.Sign(d_y) * 1;
+        } else {
+            throw new NotImplementedException();
+        }
+    }
+
+    return (h_x, h_y, t_x, t_y);
+}
+
+foreach (var ((x, y), total) in visited_locations) {
+    Console.WriteLine($"(x={x}, y={y}) = {total}");
+}
+
+Console.WriteLine($"{visited_locations.Count()}");
