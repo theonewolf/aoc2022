b6e3ffed6600ea6b1469f917598420f50e8698a2
Wolfgang Richter <wolf@wolfgroup.llc>
2022-12-13 10:52:31 -0500

    12,13: setting up the problems

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,3 +1,6 @@
+// Need to do path search from S to E.
+// Should code an algorithm from Wikipedia / algorithms book.
+
 foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
     Console.WriteLine(line);
 }
--- 13/Program.cs
+++ 13/Program.cs
@@ -1,3 +1,6 @@
+// Need to parse lists
+// Then compare left and right depth first as a tree structure
+
 foreach (string line in System.IO.File.ReadLines(@"./sample_input")) {
     Console.WriteLine(line);
 }
